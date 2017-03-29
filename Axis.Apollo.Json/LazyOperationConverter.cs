81ee17d7a32bc37bd38840ca038b2c65a4299356
Stanley Damasus <stanley.damasus@yahoo.com>
2017-03-29 13:35:13 +0100

    Added support for Deserialization of LazyOperations

---
+++ Axis.Apollo.Json/LazyOperationConverter.cs
@@ -0,0 +1,79 @@
+﻿using Axis.Luna;
+using Axis.Luna.Extensions;
+using Axis.Luna.MetaTypes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Axis.Apollo.Json
+{
+    public class LazyOperationConverter : JsonConverter
+    {
+        private static ConcurrentDictionary<string, Func<object, object>> _lazyOperationFactory = new ConcurrentDictionary<string, Func<object, object>>();
+
+        public override bool CanConvert(Type objectType) => objectType.IsGenericType && objectType.GetGenericTypeDefinition() == typeof(LazyOperation<>);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jobj = JObject.Load(reader);
+            bool? succeeded = jobj["Succeeded"]?.As<JValue>().Value.As<bool>();
+            if (succeeded == null) throw new InvalidOperationException("cannot deserialize an unresolved LazyOperation");
+
+            else if (succeeded == true) return _lazyOperationFactory.GetOrAdd(objectType.MinimalAQName() + "##", _k =>
+            {
+                //Expression: _o => Operation.ResolvedFrom<T>((T)_o)
+                var opResult = objectType.GetGenericArguments()[0];
+                var factoryMethod = typeof(Operation)
+                    .GetMethod(nameof(Operation.ResolvedFrom))
+                    .MakeGenericMethod(opResult);
+
+                var paramExp = Expression.Parameter(typeof(object), "_o");
+                var castExp = Expression.Convert(paramExp, opResult); // (T)_o
+                var callExp = Expression.Call(factoryMethod, castExp); //Operation.ResolvedFrom<T>((T)_o);
+                var lambda = Expression.Lambda(typeof(Func<object, object>), callExp, paramExp); //_o => Operation.ResolvedFrom<T>((T)_o)
+
+                return (Func<object, object>)lambda.Compile();
+            })
+            .Invoke(jobj["Result"].As<JValue>().Value);
+
+            else return _lazyOperationFactory.GetOrAdd(objectType.MinimalAQName() + "$$", _k =>
+            {
+                //Expression: _o => Operatio.FailLazily<T>((string)_o)
+                var opResult = objectType.GetGenericArguments()[0];
+                var factoryMethod = typeof(Operation)
+                    .GetMethods()
+                    .Where(_m => _m.Name == nameof(Operation.FailLazily))
+                    .Where(_m => _m.IsStatic)
+                    .Where(_m => _m.GetParameters().Length == 1 && _m.GetParameters()[0].ParameterType == typeof(string))
+                    .FirstOrDefault()
+                    .MakeGenericMethod(opResult);
+
+                var paramExp = Expression.Parameter(typeof(object), "_o");
+                var castExp = Expression.Convert(paramExp, typeof(string)); // (string)_o
+                var callExp = Expression.Call(factoryMethod, castExp); //Operation.FailLazily<T>((string)_o);
+                var lambda = Expression.Lambda(typeof(Func<object, object>), callExp, paramExp); //_o => Operation.FailLazily<T>((string)_o)
+
+                return (Func<object, object>)lambda.Compile();
+            })
+            .Invoke(jobj["Message"]?.As<JValue>().Value ?? "unknown error");
+        }
+
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            dynamic d = value;
+            JToken.FromObject(value == null ? null : new Dictionary<string, object>
+            {
+                { nameof(LazyOperation<@void>.Result), d.Result},
+                { nameof(LazyOperation<@void>.Succeeded), d.Succeeded},
+                { nameof(LazyOperation<@void>.Message), d.Message}
+            })
+            .WriteTo(writer);
+        }
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using static Axis.Luna.Extensions.EnumerableExtensions;
 using System.Linq;
+using System.Collections.Generic;
+using Axis.Luna;
 
 namespace UnitTestProject1
 {
@@ -17,12 +19,34 @@ namespace UnitTestProject1
                 Converters = Enumerate<JsonConverter>()
                     .Append(new Axis.Apollo.Json.TimeSpanConverter())
                     .Append(new Axis.Apollo.Json.DateTimeConverter())
+                    .Append(new Axis.Apollo.Json.LazyOperationConverter())
                     .ToList(),
                 Formatting = Formatting.Indented
             };
 
-            var json = JsonConvert.SerializeObject(new Obj(), jsonSetting);
+            var op = Operation.TryLazily(() => "stuff");
+            var fop = Operation.FailLazily<string>(new Exception());
+
+            var json = JsonConvert.SerializeObject(op, jsonSetting);
             Console.WriteLine(json);
+
+            var fjson = JsonConvert.SerializeObject(fop, jsonSetting);
+            Console.WriteLine(fjson);
+
+            var start = DateTime.Now;
+            var dop = JsonConvert.DeserializeObject<LazyOperation<string>>(json, jsonSetting);
+            Console.WriteLine($"1. Deserialized in {DateTime.Now - start}");
+
+            start = DateTime.Now;
+            dop = JsonConvert.DeserializeObject<LazyOperation<string>>(fjson, jsonSetting);
+            Console.WriteLine($"2. Deserialized in {DateTime.Now - start}");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var x = JsonConvert.SerializeObject(new Dictionary<string, object> { { "stuff", 5 }, { "otherStuff", true } });
+            Console.WriteLine(x);
         }
     }
 
